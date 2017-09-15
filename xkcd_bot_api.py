9841d60c590cf2627c7c9e5407342a7ca44a04ad
Moe janmohammad <admmjanmohammad@gdt-labs.local>
2017-09-14 19:30:00 -0500

    Updated Bot
    Added use of the CiscoSparkAPI wrapper to make code cleaner and not use
    helper functions.
    A few sublt changes because of how the API wrapper expects to recieve
    data

--- xkcd_bot_api.py
+++ xkcd_bot_api.py
@@ -1,7 +1,4 @@
-#!/usr/bin/python
-# this sparkbot is just for testing. will post only to the GDT sparkbot testing room. 
 import json
-import random as r
 import requests
 from credentials import *
 from ciscosparkapi import CiscoSparkAPI
@@ -9,11 +6,10 @@ from ciscosparkapi import CiscoSparkAPI
 api = CiscoSparkAPI(access_token=api_token)
 
 xkcd_json = requests.get("http://xkcd.com/info.0.json").json()
-# formatted for Spark markown
-num = str(r.randrange(0, xkcd_json[u'num'], 1))
-xkcd_json = requests.get("http://xkcd.com/"+num+"/info.0.json").json()
-xkcd_json[u'alt'] = "**Hover Text**: " + xkcd_json[u'alt'] + ' **[permalink](http://xkcd.com/' + num + ')**'
 img= [xkcd_json[u'img']]
+# formatted for Spark markown
+num = str(xkcd_json[u'num'])
+xkcd_json[u'alt'] = "**Hover Text**: " + xkcd_json[u'alt'] + ' **[permalink](http://xkcd.com/' + num + ')**'
 # Get Rooms, Parse JSON, psot to rooms the bot is in
 rooms = api.rooms.list(type='group')
 print rooms
