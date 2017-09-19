0696c6a8d53c1a05a7ea382c9047da38aa39446b
Moe janmohammad <admmjanmohammad@gdt-labs.local>
2017-09-19 17:29:56 -0500

    😀😍😴✌️ 👀
    Added Emojis to comments to attract millenials.

--- xkcd_bot_api.py
+++ xkcd_bot_api.py
@@ -3,15 +3,16 @@ import requests
 from credentials import *
 from ciscosparkapi import CiscoSparkAPI
 
+#Create API call - 😛 🤑
 api = CiscoSparkAPI(access_token=api_token)
 
 xkcd_json = requests.get("http://xkcd.com/info.0.json").json()
+# API wrapper expects a list of files. We made it a list. 🖕
 img= [xkcd_json[u'img']]
-# formatted for Spark markown
+# formatted for Spark markown 💩
 num = str(xkcd_json[u'num'])
 xkcd_json[u'alt'] = "**Hover Text**: " + xkcd_json[u'alt'] + ' **[permalink](http://xkcd.com/' + num + ')**'
-# Get Rooms, Parse JSON, psot to rooms the bot is in
+# Get Rooms, Parse JSON, psot to rooms the bot is in 🗣 👤 👥 
 rooms = api.rooms.list(type='group')
-print rooms
 for room in rooms:
     api.messages.create(roomId=room.id,markdown=xkcd_json[u'alt'], files=img)
--- xkcd_bot_random_api.py
+++ xkcd_bot_random_api.py
@@ -6,16 +6,17 @@ import requests
 from credentials import *
 from ciscosparkapi import CiscoSparkAPI
 
+#Create API call - 😛 🤑
 api = CiscoSparkAPI(access_token=api_token)
 
 xkcd_json = requests.get("http://xkcd.com/info.0.json").json()
-# formatted for Spark markown
+# formatted for Spark markown 💩
 num = str(r.randrange(0, xkcd_json[u'num'], 1))
 xkcd_json = requests.get("http://xkcd.com/"+num+"/info.0.json").json()
 xkcd_json[u'alt'] = "**Hover Text**: " + xkcd_json[u'alt'] + ' **[permalink](http://xkcd.com/' + num + ')**'
+# API wrapper expects a list of files. We made it a list. 🖕
 img= [xkcd_json[u'img']]
-# Get Rooms, Parse JSON, psot to rooms the bot is in
+# Get Rooms, Parse JSON, psot to rooms the bot is in 🗣 👤 👥 
 rooms = api.rooms.list(type='group')
-print rooms
 for room in rooms:
     api.messages.create(roomId=room.id,markdown=xkcd_json[u'alt'], files=img)
